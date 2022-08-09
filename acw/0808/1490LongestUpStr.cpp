d03458db14cd6e3ce3cc1b56a93557d04d682ced
XueSongTap <tapkindle@outlook.com>
2022-08-09 18:02:55 +0800

    0809 from work

---
+++ acw/0808/1490LongestUpStr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <algorihtm>
+
+#include <cstdio>
+
+
+using namespace std;
+
+
+const int N =  
+
+int main(){
+
+    int n;
+
+    cin >> n;
+
+    for (int i = )
+
+}
